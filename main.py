4abd65fb59facf8b72c9caea669f702b853dc22b
Mykola1601 <nik160186@gmail.com>
2023-10-03 19:21:08 +0300

    1921

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ class Name(Field):
 
 
 class Phone(Field):
-    # реалізація класу
+    # реалізація класу 10 digit
+    # Реалізовано валідацію номера телефону(має бути 10 цифр).
     ...
 
 
@@ -26,7 +27,25 @@ class Record:
         self.name = Name(name)
         self.phones = []
 
-    # реалізація класу
+
+    def add_phone():
+        ...
+
+    def remove_phone():
+        ...
+
+    def edit_phone():
+        ...
+
+    def find_phone():
+        ...
+
+
+    # Реалізовано зберігання об'єкта Name в окремому атрибуті.
+    # Реалізовано зберігання списку об'єктів Phone в окремому атрибуті.
+    # Реалізовано методи для додавання - add_phone/видалення -
+    # remove_phone/редагування - edit_phone/пошуку об'єктів Phone -
+    # find_phone.
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
@@ -34,6 +53,22 @@ class Record:
 
 class AddressBook(UserDict):
     # реалізація класу
+    # Додавання записів.
+    # Пошук записів за іменем.
+    # Видалення записів за іменем.
+    def add_record():
+        ...
+
+    def find():
+        ...
+
+    def delete():
+        ...
+    # Реалізовано метод add_record, який додає запис до self.data.
+    # Реалізовано метод find, який знаходить запис за ім'ям.
+    # Реалізовано метод delete, який видаляє запис за ім'ям.
+    # Записи Record у AddressBook зберігаються як значення у словнику.
+    # В якості ключів використовується значення Record.name.value.
     ...
 
 
